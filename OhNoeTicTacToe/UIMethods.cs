66c42b9fc03ced17e3e610d695d015ac783e66a4
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-11-30 15:11:31 -0600

    put the code in the proper files

--- OhNoeTicTacToe/UIMethods.cs
+++ OhNoeTicTacToe/UIMethods.cs
@@ -10,7 +10,36 @@ namespace OhNoeTicTacToe
     {
         public static void DisplayTicTacToeBoard()
         {
-            Console.WriteLine("Welcome to TicTacToe!");
+            const int BOARD_SIZE = 3;
+            char[,] board;
+            board = new char[BOARD_SIZE, BOARD_SIZE];
+
+            for (int row = 0; row < BOARD_SIZE; row++)
+            {
+                for (int col = 0; col < BOARD_SIZE; col++)
+                {
+                    board[row, col] = ' ';
+                }
+            }
+
+            for (int row = 0; row < BOARD_SIZE; row++)
+            {
+                for (int col = 0; col < BOARD_SIZE; col++)
+                {
+                    Console.Write($"{board[row, col]}");
+                    if (col < BOARD_SIZE - 1) Console.Write(" | ");
+                }
+                Console.WriteLine();
+                if (row < BOARD_SIZE - 1)
+                {
+                    for (int i = 0; i < BOARD_SIZE; i++)
+                    {
+                        Console.Write("---");
+                        if (i < BOARD_SIZE - 1) Console.Write("+");
+                    }
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
