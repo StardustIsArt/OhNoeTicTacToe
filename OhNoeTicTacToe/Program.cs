8894c21a25a4eb39b0565d04af9b5b117a8b0d8a
YociusProductions <90871690+StardustIsArt@users.noreply.github.com>
2025-11-30 12:21:18 -0600

    created files

---
+++ OhNoeTicTacToe/Program.cs
@@ -0,0 +1,14 @@
+﻿using OhNoeTicTacToe;
+
+UIMethods.DisplayTicTacToe();
+
+int size = 0;
+board = new char[size, size];
+
+for (int row = 0; row < size; row++)
+{
+    for (int col = 0; col < size; col++)
+    {
+        board[row, col] = ' ';
+    }
+}
---
+++ OhNoeTicTacToe/UIMethods.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OhNoeTicTacToe
+{
+    public static class UIMethods
+    {
+        public static void DisplayTicTacToe()
+        {
+            Console.WriteLine("Welcome to TicTacToe!");
+        }
+    }
+}
